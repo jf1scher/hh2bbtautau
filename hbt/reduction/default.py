5948b9bb466adf9624fce5f9360768962b0d0605
Jonah Fischer <fisjonah@naf-cms-gpu01.desy.de>
2025-07-14 13:35:20 +0200

    Merge.

--- hbt/reduction/default.py
+++ hbt/reduction/default.py
@@ -8,6 +8,7 @@ from columnflow.reduction import Reducer, reducer
 from columnflow.reduction.default import cf_default
 from columnflow.production.cms.dy import gen_dilepton, recoil_corrected_met
 from columnflow.production.cms.top_pt_weight import gen_parton_top as cf_gen_parton_top
+from hbt.production.higgs_decay_products import higgs_decay_products
 from columnflow.util import maybe_import
 
 from hbt.util import IF_DATASET_HAS_TOP, IF_DATASET_IS_DY
@@ -23,16 +24,20 @@ gen_parton_top = cf_gen_parton_top.derive("gen_parton_top", cls_dict={"require_d
         cf_default,
         IF_DATASET_HAS_TOP(gen_parton_top),
         IF_DATASET_IS_DY(gen_dilepton, recoil_corrected_met),
+        higgs_decay_products,
     },
     produces={
         cf_default,
         IF_DATASET_HAS_TOP(gen_parton_top),
         IF_DATASET_IS_DY(gen_dilepton, recoil_corrected_met),
+        higgs_decay_products,
+	'GenPart.*'
     },
 )
 def default(self: Reducer, events: ak.Array, selection: ak.Array, **kwargs) -> ak.Array:
     # run cf's default reduction which handles event selection and collection creation
     events = self[cf_default](events, selection, **kwargs)
+    events = self[higgs_decay_products](events, **kwargs)
 
     # add generator particles, depending on the dataset
     if self.has_dep(gen_parton_top):
